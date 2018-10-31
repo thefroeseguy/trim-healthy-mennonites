c1fbb0bf87031cebbed27c7dc7db7ea8922fde9d
thefroeseguy <33730048+thefroeseguy@users.noreply.github.com>
2018-10-31 07:38:16 -0400

    Update js/recipes.js

--- js/recipes.js
+++ js/recipes.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     const recipes = document.querySelectorAll('.recipes');
+    $(recipes).hide();
     $('.recipe-nav').on('click', function(e) {
         const navIndex = $(e.target).parent().parent().index();
         $('.recipes').fadeOut();
